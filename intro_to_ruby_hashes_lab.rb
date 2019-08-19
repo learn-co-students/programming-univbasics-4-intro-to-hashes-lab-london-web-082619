20738b55095af44f070692fbe3e77f9fd5cbaaef
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-19 11:38:31 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,30 @@
 def new_hash
-  # return an empty hash
+  hash = {}# return an empty hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash ={
+    takeshi: 3,
+    david: 4,
+    tom: 5
+}# return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name: "Grace Hopper"}
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  hash ={
+    id: 4}
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash = {}
+  hash.store(key, value)
+  hash
+   # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
